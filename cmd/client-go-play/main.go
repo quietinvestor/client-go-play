4d7f5ee1252c616c27b19f7216f43ca5508f5c07
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2025-01-08 18:25:32 +0100

    Remove kubeclient package and run()

--- cmd/client-go-play/main.go
+++ cmd/client-go-play/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/quietinvestor/client-go-play/internal/kubeclient"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 	"k8s.io/klog/v2/textlogger"
 )
 
@@ -17,47 +21,51 @@ const (
 	defaultTimeout = 10 * time.Second
 )
 
-func setupClient(ctx context.Context, kubeconfig string) (kubeclient.Interface, error) {
-	logger, err := logr.FromContext(ctx)
-	if err != nil {
-		config := textlogger.NewConfig()
-		logger = textlogger.NewLogger(config).WithName("setup-client")
-		logger.V(2).Info("No logger found in context, created new logger")
+func setupClient(ctx context.Context, kubeconfig string) (*kubernetes.Clientset, error) {
+	logger, _ := logr.FromContext(ctx)
+
+	if kubeconfig == "" {
+		home := homedir.HomeDir()
+		if home == "" {
+			logger.Error(errors.New("home directory not found"), "Failed to create client")
+			return nil, fmt.Errorf("Failed to create client: home directory not found")
+		}
+		kubeconfig = filepath.Join(home, ".kube", "config")
 	}
 
-	clientset, err := kubeclient.New(kubeconfig)
+	logger = logger.WithValues("kubeconfig", kubeconfig)
+
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		logger.Error(err, "Failed to create clientset",
-			"kubeconfig", kubeconfig)
+		logger.Error(err, "Failed to load kubeconfig")
+		return nil, fmt.Errorf("Failed to load kubeconfig from %s: %w", kubeconfig, err)
+	}
+
+	logger.V(2).Info("Successfully loaded kubeconfig")
+
+	clientset, err := kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		logger.Error(err, "Failed to create clientset")
 		return nil, fmt.Errorf("Failed to create clientset: %w", err)
 	}
 
-	logger.V(2).Info("Successfully created clientset",
-		"kubeconfig", kubeconfig)
+	logger.V(2).Info("Successfully created clientset")
 
 	return clientset, nil
 }
 
-func listPods(ctx context.Context, clientset kubeclient.Interface, namespace string, opts metav1.ListOptions) error {
-	logger, err := logr.FromContext(ctx)
-	if err != nil {
-		config := textlogger.NewConfig()
-		logger = textlogger.NewLogger(config).WithName("list-pods")
-		logger.V(2).Info("No logger found in context, created new logger")
-	}
+func listPods(ctx context.Context, clientset *kubernetes.Clientset, namespace string, opts metav1.ListOptions) error {
+	logger, _ := logr.FromContext(ctx)
+	logger = logger.WithValues("namespace", namespace, "opts", opts)
 
-	podList, err := clientset.ClientSet().CoreV1().Pods(namespace).List(ctx, opts)
+	podList, err := clientset.CoreV1().Pods(namespace).List(ctx, opts)
 	if err != nil {
 		logger.Error(err, "Failed to list pods",
-			"namespace", namespace,
-			"opts", opts,
 			"podsCount", len(podList.Items))
 		return fmt.Errorf("Failed to list pods: %w", err)
 	}
 
 	logger.V(2).Info("Successfully listed pods",
-		"namespace", namespace,
-		"opts", opts,
 		"podsCount", len(podList.Items))
 
 	for _, pod := range podList.Items {
@@ -65,15 +73,6 @@ func listPods(ctx context.Context, clientset kubeclient.Interface, namespace str
 	}
 
 	return nil
-}
-
-func run(ctx context.Context, kubeconfig, namespace string, opts metav1.ListOptions) error {
-	clientset, err := setupClient(ctx, kubeconfig)
-	if err != nil {
-		return err
-	}
-
-	return listPods(ctx, clientset, namespace, opts)
 }
 
 func main() {
@@ -88,7 +87,12 @@ func main() {
 	namespace := ""
 	opts := metav1.ListOptions{}
 
-	if err := run(ctx, kubeconfig, namespace, opts); err != nil {
+	clientset, err := setupClient(ctx, kubeconfig)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := listPods(ctx, clientset, namespace, opts); err != nil {
 		os.Exit(1)
 	}
 }
