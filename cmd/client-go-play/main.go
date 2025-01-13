bc94ebe656e23c95929632474cdeb3697832c0c9
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2025-01-13 18:55:02 +0100

    Refactor CLI commands into cmd package

--- cmd/client-go-play/main.go
+++ cmd/client-go-play/main.go
@@ -1,89 +1,13 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"os"
-	"time"
 
-	"github.com/quietinvestor/client-go-play/internal/kubeclient"
-	"github.com/quietinvestor/client-go-play/internal/pods"
-
-	"github.com/go-logr/logr"
-	"github.com/spf13/cobra"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/klog/v2/textlogger"
-)
-
-const (
-	defaultTimeout = 10 * time.Second
+	"github.com/quietinvestor/client-go-play/internal/cmd"
 )
 
 func main() {
-	var cancel context.CancelFunc
-	var client *kubernetes.Clientset
-	var config *rest.Config
-	var ctx context.Context
-	var namespace, path string
-	var opts metav1.ListOptions
-
-	loggerConfig := textlogger.NewConfig()
-	configFlags := flag.NewFlagSet("k8s", flag.ExitOnError)
-
-	rootCmd := &cobra.Command{
-		Use:   "k8s",
-		Short: "Kubernetes client examples",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-
-			logger := textlogger.NewLogger(loggerConfig).WithName("k8s-client")
-
-			ctx = logr.NewContext(context.Background(), logger)
-			ctx, cancel = context.WithTimeout(ctx, defaultTimeout)
-
-			config, err = kubeclient.NewConfig(ctx, path)
-			if err != nil {
-				return err
-			}
-
-			client, err = kubeclient.NewClient(ctx, config)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		},
-		PersistentPostRun: func(cmd *cobra.Command, args []string) {
-			cancel()
-		},
-	}
-
-	podsCmd := &cobra.Command{
-		Use:   "pods",
-		Short: "Pod operations",
-	}
-
-	listCmd := &cobra.Command{
-		Use:   "list",
-		Short: "List pods",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return pods.List(ctx, client, namespace, opts)
-		},
-	}
-
-	loggerConfig.AddFlags(configFlags)
-	rootCmd.PersistentFlags().AddGoFlagSet(configFlags)
-	rootCmd.PersistentFlags().StringVarP(&path, "kubeconfig", "k", "", "kubeconfig file path")
-	rootCmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "", "namespace to filter resources")
-
-	podsCmd.AddCommand(listCmd)
-	rootCmd.AddCommand(podsCmd)
-
-	opts = metav1.ListOptions{}
-
+	rootCmd := cmd.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
---
+++ internal/cmd/cmd.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"github.com/quietinvestor/client-go-play/internal/kubeclient"
+	"github.com/quietinvestor/client-go-play/internal/pods"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog/v2/textlogger"
+)
+
+const defaultTimeout = 10 * time.Second
+
+type contextKey string
+
+const stateKey = contextKey("state")
+
+type State struct {
+	Ctx       context.Context
+	Cancel    context.CancelFunc
+	Client    *kubernetes.Clientset
+	Namespace string
+	Opts      metav1.ListOptions
+}
+
+func NewRootCmd() *cobra.Command {
+	var namespace, path string
+
+	loggerConfig := textlogger.NewConfig()
+	configFlags := flag.NewFlagSet("k8s", flag.ExitOnError)
+
+	cmd := &cobra.Command{
+		Use:   "k8s",
+		Short: "Kubernetes client examples",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+
+			logger := textlogger.NewLogger(loggerConfig).WithName("k8s-client")
+
+			ctx := logr.NewContext(context.Background(), logger)
+			ctx, cancel := context.WithTimeout(ctx, defaultTimeout)
+
+			config, err := kubeclient.NewConfig(ctx, path)
+			if err != nil {
+				cancel()
+				return err
+			}
+
+			client, err := kubeclient.NewClient(ctx, config)
+			if err != nil {
+				cancel()
+				return err
+			}
+
+			state := &State{
+				Ctx:       ctx,
+				Cancel:    cancel,
+				Client:    client,
+				Namespace: namespace,
+				Opts:      metav1.ListOptions{},
+			}
+
+			cmd.SetContext(context.WithValue(ctx, stateKey, state))
+
+			return nil
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			state := cmd.Context().Value(stateKey).(*State)
+			state.Cancel()
+		},
+	}
+
+	loggerConfig.AddFlags(configFlags)
+	cmd.PersistentFlags().AddGoFlagSet(configFlags)
+	cmd.PersistentFlags().StringVarP(&path, "kubeconfig", "k", "", "kubeconfig file path")
+	cmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "", "namespace to filter resources")
+
+	cmd.AddCommand(newPodsCmd())
+
+	return cmd
+}
+
+func newPodsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "pods",
+		Short: "Pod operations",
+	}
+
+	cmd.AddCommand(newPodsListCmd())
+
+	return cmd
+}
+
+func newPodsListCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List pods",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			state := cmd.Context().Value(stateKey).(*State)
+			return pods.List(state.Ctx, state.Client, state.Namespace, state.Opts)
+		},
+	}
+
+	return cmd
+}
